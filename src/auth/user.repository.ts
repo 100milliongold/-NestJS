e20bbee8b8007b9bb703c2ae1d56f1da816de48f
100milliongold <gadian88@gmail.com>
2021-10-01 17:08:34 +0900

    비밀번호 암호화 하기

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -5,16 +5,20 @@ import {
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   /**
    * 회원 가입
-   * @param authCredentialsDto 클라이언트에서 입력되는 내용
+   * @param authCredentialsDto 클라이언트에서 *입력되는 내용
    */
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-    const user = this.create({ username, password });
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = this.create({ username, password: hashedPassword });
 
     try {
       await this.save(user);
